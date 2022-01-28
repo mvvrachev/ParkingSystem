327a3922ef7f051aea4abd0512e6eb79d10fab57
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2022-01-28 11:28:04 +0200

    Added navigation library and created login and register fragments

--- app/src/main/java/com/example/parkingsystem/MainActivity.kt
+++ app/src/main/java/com/example/parkingsystem/MainActivity.kt
@@ -2,14 +2,31 @@ package com.example.parkingsystem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.parkingsystem.databinding.ActivityMainBinding
 
 // TODO: create authentication activity
 // TODO: add Navigation Library in order to navigate between fragments
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
