a23fa9571587a531af3cff9804ade9b009d59880
mvvrachev <martin.v.vrachev.2017@elsys-bg.org>
2022-04-04 00:01:08 +0300

    Remove excess comment

--- app/src/main/java/com/example/parkingsystem/utils/EmailSender.java
+++ app/src/main/java/com/example/parkingsystem/utils/EmailSender.java
@@ -1,125 +1,12 @@
 package com.example.parkingsystem.utils;
-//
-//import android.util.Log;
-//
-//import androidx.annotation.NonNull;
-//
-//import com.example.parkingsystem.models.Reservation;
-//import com.google.android.gms.tasks.OnCompleteListener;
-//import com.google.android.gms.tasks.Task;
-//import com.google.firebase.firestore.FirebaseFirestore;
-//import com.google.firebase.firestore.QueryDocumentSnapshot;
-//import com.google.firebase.firestore.QuerySnapshot;
-//
-//import java.net.PasswordAuthentication;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Properties;
-//
-//import javax.mail.Message;
-//import javax.mail.MessagingException;
-//import javax.mail.Session;
-//import javax.mail.Transport;
-//import javax.mail.internet.InternetAddress;
-//import javax.mail.internet.MimeMessage;
-//
-//public class EmailSender {
-//
-//    public static void sendEmail() {
-//
-//        // Recipient's email ID needs to be mentioned.
-//        String to = "parkingsystem4@gmail.com";
-//
-//        // Sender's email ID needs to be mentioned
-//        String from = "parkingsystem4@gmail.com";
-//
-//        // Assuming you are sending email from through gmails smtp
-//        String host = "smtp.gmail.com";
-//
-//        // Get system properties
-//        Properties properties = System.getProperties();
-//
-//        // Setup mail server
-//        properties.put("mail.smtp.host", host);
-//        properties.put("mail.smtp.port", "465");
-//        properties.put("mail.smtp.ssl.enable", "true");
-//        properties.put("mail.smtp.auth", "true");
-//
-//        // Get the Session object.// and pass username and password
-//        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-//
-//            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
-//
-////                String password = "parkingsystem123";
-////                char[] passwordChar = password.toCharArray();
-//                return new javax.mail.PasswordAuthentication("parkingsystem4@gmail.com", "parkingsystem123");
-//
-//            }
-//
-//        });
-//
-//        try {
-//            // Create a default MimeMessage object.
-//            MimeMessage message = new MimeMessage(session);
-//
-//            // Set From: header field of the header.
-//            message.setFrom(new InternetAddress(from));
-//
-//            // Set To: header field of the header.
-//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-//
-//            // Set Subject: header field
-//            message.setSubject("This is the Subject Line!");
-//
-////            List<Reservation> r = new ArrayList<>();
-////            r = getReservationsInformation();
-//
-//            // Now set the actual message
-//            message.setText("message");
-//
-//            System.out.println("sending...");
-//            // Send message
-//            Transport.send(message);
-//            System.out.println("Sent message successfully....");
-//        } catch (MessagingException mex) {
-//            mex.printStackTrace();
-//        }
-//    }
-//
-//    private static List<Reservation> getReservationsInformation() {
-//        List<Reservation> reservations = new ArrayList();
-//        FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        db.collection("reservations")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                reservations.add(document.toObject(Reservation.class));
-//                            }
-//                        } else {
-//                            Log.d("tag", "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                });
-//
-//        return reservations;
-//    }
-//
-//}
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
